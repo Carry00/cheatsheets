9c7507bdaa030ad656297243d9a01e1091bd50be
carry00 <carry00@github.com>
2024-08-04 16:01:36 +0800

    notes

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
-# 映射 URL 的接口
-mapping_url="https://gist.githubusercontent.com/Carry00/6e526b8751f667f667a9be540d015939/raw/fd8b0224f17ef3c4d2d49e2196bafc2f82dba1a1/command_mapping"
-
-# 函数：获取所有命令及其对应的 Gist URL
-get_all_commands() {
-  curl -s "${mapping_url}" | jq -r 'keys[]'
-}
-
-# 函数：获取命令对应的 Gist URL
-get_gist_url() {
-  command=$1
-  curl -s "${mapping_url}" | jq -r --arg cmd "$command" '.[$cmd]'
-}
-
-# 检查输入参数
+# 检查是否传入参数
 if [ $# -eq 0 ]; then
-  # 获取所有命令
-  commands=$(get_all_commands)
-  echo "Available commands:"
-  echo "$commands" | xargs -n 10
-  exit 0
-elif [ $# -ne 1 ]; then
-  echo "Usage: $0 <command>"
-  exit 1
+    echo "Usage: $0 <parameter>"
+    exit 1
 fi
 
-# 获取输入命令
-command=$1
+# 获取传入的参数
+param=$1
 
-# 获取对应的 Gist URL
-gist_url=$(get_gist_url "$command")
+# 构建URL
+url="https://raw.githubusercontent.com/Carry00/cheatsheets/master/${param}"
 
-if [ -z "$gist_url" ]; then
-  echo "Command not found."
-  exit 1
-fi
+# 使用curl请求URL并输出内容
+response=$(curl -s "$url")
 
-# 使用 curl 请求 Gist 内容并显示
-curl -s "$gist_url"
+# 输出响应内容
+echo "$response"
